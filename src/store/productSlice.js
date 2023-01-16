725835cf78e3a5321cef074c927f22da17f53c42
Rohith <marganirohith2000@gmail.com>
2023-01-16 16:07:04 +0530

    redux async programming with redux toolkit syntax

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,6 +1,6 @@
 import { act, findRenderedComponentWithType } from "react-dom/test-utils";
 import axios from "axios";
-const { createSlice } = require("@reduxjs/toolkit");
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
 
 export const STATUSES = Object.freeze({
   IDLE: "idle",
@@ -12,15 +12,28 @@ const productSlice = createSlice({
   name: "product",
   initialState,
   reducers: {
-    setProducts(state, action) {
-      state.data = action.payload;
-    },
-    // remove(state, action) {
-    //   return state.filter((item) => item.id !== action.payload);
+    // these reducers were needed for basic syntax of async programming in redux
+    // setProducts(state, action) {
+    //   state.data = action.payload;
     // },
-    setStatus(state, action) {
-      state.status = action.payload;
-    },
+    // setStatus(state, action) {
+    //   state.status = action.payload;
+    // },
+  },
+  //  these are needed for redux-toolkit syntax of redux async programming
+  extraReducers: (builder) => {
+    builder
+      //creating 3 actions for async thunk .. ie pending ,fullfiled , rejected
+      .addCase(fetchProducts.pending, (state, action) => {
+        state.status = STATUSES.LOADING;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.status = STATUSES.IDLE;
+      })
+      .addCase(fetchProducts.rejected, (state) => {
+        state.status = STATUSES.ERROR;
+      });
   },
 });
 
@@ -28,20 +41,29 @@ export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
 //thunks
-export function fetchProducts() {
-  return async function fetchProductThunk(dispatch, getState) {
-    //getSate is to get current state
-    dispatch(setStatus(STATUSES.LOADING));
-    // const state = getState();
-    // console.log(state);
-    try {
-      const res = await axios.get("https://fakestoreapi.com/products");
-      const data = res.data;
-      dispatch(setProducts(data));
-      dispatch(setStatus(STATUSES.IDLE));
-    } catch (err) {
-      console.log(err);
-      dispatch(setStatus(STATUSES.ERROR));
-    }
-  };
-}
+
+// this redux-toolkit syntax
+// product/fetch is identifier
+export const fetchProducts = createAsyncThunk("product/fetch", async () => {
+  const res = await axios.get("https://fakestoreapi.com/products");
+  const data = res.data;
+  return data;
+});
+
+// export function fetchProducts() {
+//   return async function fetchProductThunk(dispatch, getState) {
+//     //getSate is to get current state
+//     dispatch(setStatus(STATUSES.LOADING));
+//     // const state = getState();
+//     // console.log(state);
+//     try {
+//       const res = await axios.get("https://fakestoreapi.com/products");
+//       const data = res.data;
+//       dispatch(setProducts(data));
+//       dispatch(setStatus(STATUSES.IDLE));
+//     } catch (err) {
+//       console.log(err);
+//       dispatch(setStatus(STATUSES.ERROR));
+//     }
+//   };
+// }
